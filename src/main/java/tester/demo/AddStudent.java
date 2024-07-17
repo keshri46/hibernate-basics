bcbab206705d49205e061ccc3510ff8e7c8847ad
keshri46 <keshri1613@gmail.com>
2024-07-17 17:59:08 +0530

    Added Files

--- src/main/java/tester/demo/AddStudent.java
+++ src/main/java/tester/demo/AddStudent.java
@@ -3,18 +3,22 @@ package tester.demo;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/add")
+@MultipartConfig
 public class AddStudent extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		byte[] file = new byte[req.getPart("file").getInputStream().available()];
+		req.getPart("file").getInputStream().read(file);
 		Student s = new Student(req.getParameter("name"), req.getParameter("address"),
-				Double.parseDouble(req.getParameter("percentage")), req.getParameter("gender"));
+				Double.parseDouble(req.getParameter("percentage")), req.getParameter("gender"), file);
 		HibernateLogics.addStudent(s);
 		resp.getWriter().println("<h1>Student Added</h1>");
 //		resp.getWriter().println("<a href=\"home.html\"><button>Home</button></a>");
--- src/main/java/tester/demo/Student.java
+++ src/main/java/tester/demo/Student.java
@@ -4,13 +4,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import lombok.Data;
 
 @Data
 @Entity
 public class Student {
-	
+
 	@Id
 	@GeneratedValue
 	private int sId;
@@ -19,17 +20,20 @@ public class Student {
 	private String sAddress;
 	private double sPercentage;
 	private String sGender;
-	
-	public Student(String sName, String sAddress, double sPercentage, String sGender) {
+	@Lob
+	private byte[] file;
+
+	public Student(String sName, String sAddress, double sPercentage, String sGender, byte[] file) {
 		super();
 		this.sName = sName;
 		this.sAddress = sAddress;
 		this.sPercentage = sPercentage;
 		this.sGender = sGender;
+		this.file = file;
 	}
-	
+
 	public Student() {
-		
+
 	}
-	
+
 }
